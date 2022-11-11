90100ae76efaac1560301467792156869568e0b8
Spencer Chisholm <spencer.chisholm@live.ca>
2022-11-11 22:36:43 +0000

    Javascripting workshop done

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function doMath(a, b, c){
+  return a + b * c;
+}
+
+console.log(doMath(53, 61, 67));
---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,7 @@
+let animals = ['cat', 'dog', 'rat'];
+
+for ( let i = 0; i < animals.length; i++){
+  animals[i] += 's';
+}
+
+console.log(animals);
